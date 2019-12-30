3420d77bf28b2c542cfa6b34aab43c62ffae3e45
Erik Anderson <eanderson@enviroincentives.com>
2019-12-30 08:57:37 -0700

    configure .bat file for local machine and add to .gitignore

--- run-util-report.bat
+++ run-util-report.bat
@@ -1,2 +1,2 @@
-"C:\Users\Erik\Anaconda3\python.exe" "C:\Users\Erik\Documents\_c-dev\utilization-tracker\scripts\data-pipe.py"
+"C:\Users\Erik\Anaconda3\python.exe" "C:\Users\Erik\Documents\_data-products\utilization-tracker\scripts\data-pipe.py"
 pause
